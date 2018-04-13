afe29410371adde09e2e92ba38013688be6f2143
Dan Fauxsmith <dfauxsmith@oreilly.com>
2018-04-13 14:25:32 -0400

    adding files

---
+++ lesson4.py
@@ -0,0 +1,47 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.datasets import make_blobs
+from sklearn.svm import SVC
+
+
+def plot_svc_decision_function(model, ax=None, plot_support=True):
+    """Plot the decision function for a 2D SVC"""
+    if ax is None:
+        ax = plt.gca()
+    xlim = ax.get_xlim()
+    ylim = ax.get_ylim()
+    
+    # create grid to evaluate model
+    x = np.linspace(xlim[0], xlim[1], 30)
+    y = np.linspace(ylim[0], ylim[1], 30)
+    Y, X = np.meshgrid(y, x)
+    xy = np.vstack([X.ravel(), Y.ravel()]).T
+    P = model.decision_function(xy).reshape(X.shape)
+    
+    # plot decision boundary and margins
+    ax.contour(X, Y, P, colors='k',
+               levels=[-1, 0, 1], alpha=0.5,
+               linestyles=['--', '-', '--'])
+    
+    # plot support vectors
+    if plot_support:
+        ax.scatter(model.support_vectors_[:, 0],
+                   model.support_vectors_[:, 1],
+                   s=300, linewidth=1, facecolors='none');
+    ax.set_xlim(xlim)
+    ax.set_ylim(ylim)
+
+    
+def plot_svm(N=10, ax=None):
+    X, y = make_blobs(n_samples=200, centers=2,
+                      random_state=0, cluster_std=0.60)
+    X = X[:N]
+    y = y[:N]
+    model = SVC(kernel='linear', C=1E10)
+    model.fit(X, y)
+    
+    ax = ax or plt.gca()
+    ax.scatter(X[:, 0], X[:, 1], c=y, s=50, cmap='autumn')
+    ax.set_xlim(-1, 4)
+    ax.set_ylim(-1, 6)
+    plot_svc_decision_function(model, ax)
